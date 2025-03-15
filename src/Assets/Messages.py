7131b2901d063b9d7aaaba3a3d694337f2d4eb4d
farzadoxo <farzadebrahimi.oxo@gmail.com>
2025-03-15 21:03:49 +0330

    assets added

---
+++ src/Assets/Messages.py
@@ -0,0 +1,7 @@
+class EventMessage:
+
+    on_connect = ">>> TheBot successfully connected to Discord API"
+
+    on_resumed = ">>> TheBot successfully resumed the connection"
+
+    on_ready = ">>> TheBot is ready to use"
